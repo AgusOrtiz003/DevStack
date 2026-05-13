3c275b71992c6381d5a744b3ff3bb88ce91c48bd
Santiago <santiagogabriel.rojaszapata@gmail.com>
2026-05-13 16:25:50 -0300

    Actualizando el main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from nicegui import ui
 
 # Acá irian todos los import de páginas .py que se tienen que ejecutar
-from frontend.pages import listar_reservas_pendientes
-from frontend.pages import home, listar_turnos, listar_turnos_pacientes, registar_turno, reservas
+from frontend.pages import home
+# Importar páginas de reserva
+from frontend.pages.reservas import cancelar_reserva,listar_reservas,modificar_reserva,registar_reserva
+# Importar páginas de turnos
+from frontend.pages.turnos import listar_turnos,modificar_turno,registar_turno
 
 ui.run(language='es')
