decd264d99dc5d45451c16cef79894b9d35ad1a8
Agustin Ortiz <agusortiz003@gmail.com>
2026-05-13 19:59:00 -0300

    Agrego registro y login

---
+++ src/front-end/login.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from nicegui import app, ui
+
+# in reality users passwords would obviously need to be hashed
+passwords = {'user1': 'pass1', 'user2': 'pass2'}
+
+# top-level static routes like /favicon.ico must be unrestricted, otherwise the middleware redirects them to /login
+unrestricted_page_routes = {'/favicon.ico', '/login'}
+
+@app.add_middleware
+class AuthMiddleware(BaseHTTPMiddleware):
+    """This middleware restricts access to all NiceGUI pages.
+
+    It redirects the user to the login page if they are not authenticated.
+    """
+
+    async def dispatch(self, request: Request, call_next):
+        path = request.url.path
+        if app.storage.user.get('authenticated') or path in unrestricted_page_routes or path.startswith('/_nicegui'):
+            return await call_next(request)
+        return RedirectResponse(f'/login?redirect_to={path}')
+
+@ui.page('/')
+def main_page() -> None:
+    def logout() -> None:
+        app.storage.user.clear()
+        ui.navigate.to('/login')
+
+    with ui.column().classes('absolute-center items-center'):
+        ui.label(f'Hello {app.storage.user["username"]}!').classes('text-2xl')
+        ui.button(on_click=logout, icon='logout').props('outline round')
+
+@ui.page('/subpage')
+def test_page() -> None:
+    ui.label('This is a sub page.')
+
+@ui.page('/login')
+def login(redirect_to: str = '/') -> RedirectResponse | None:
+    if app.storage.user.get('authenticated'):
+        return RedirectResponse('/')
+
+    def try_login() -> None:
+        if passwords.get(username.value) == password.value:
+            app.storage.user.update(username=username.value, authenticated=True)
+            ui.navigate.to(redirect_to)  # go back to where the user wanted to go
+        else:
+            ui.notify('Nombre de Usuario o Contraseña incorrectos', color='negative')
+
+    with ui.card().classes('absolute-center items-stretch'):
+        username = ui.input('Nombre de Usuario').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
+        password = ui.input('Contraseña', password=True, password_toggle_button=True).on('keydown.enter', try_login)
+        ui.button('Log in', on_click=try_login)
+        ui.button('Register', on_click=None)
+
+    return None
+
+@ui.page('/register')
+    
+if __name__ in {'__main__', '__mp_main__'}:
+    ui.run(storage_secret='THIS_NEEDS_TO_BE_CHANGED')
