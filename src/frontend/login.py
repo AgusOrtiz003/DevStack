7d01a82a137877445cb3a5167faac815fc356b98
Agustin Ortiz <agusortiz003@gmail.com>
2026-05-14 17:02:10 -0300

    Actualizo el frontend de login

--- src/frontend/login.py
+++ src/frontend/login.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-
+import pathlib
+import sys
+import sqlite3
 from fastapi import Request
 from fastapi.responses import RedirectResponse
 from starlette.middleware.base import BaseHTTPMiddleware
-
+src_path=pathlib.Path(__file__).resolve().parent.parent
+sys.path.append(str(src_path))
 from nicegui import app, ui
-from src.backend import registro
-
+from backend.registro import registrar
+from backend.login import chequearContraseña
 # in reality users passwords would obviously need to be hashed
 passwords = {'user1': 'pass1', 'user2': 'pass2'}
 
 # top-level static routes like /favicon.ico must be unrestricted, otherwise the middleware redirects them to /login
-unrestricted_page_routes = {'/favicon.ico', '/login'}
+unrestricted_page_routes = {'/favicon.ico', '/login','/register'}
 
 @app.add_middleware
 class AuthMiddleware(BaseHTTPMiddleware):
@@ -36,39 +39,49 @@ def main_page() -> None:
         ui.label(f'Hello {app.storage.user["username"]}!').classes('text-2xl')
         ui.button(on_click=logout, icon='logout').props('outline round')
 
-@ui.page('/subpage')
-def test_page() -> None:
-    ui.label('This is a sub page.')
+## VOY A HACER QUE SE LOGUEEN CON SU DNI
+@ui.page('/register')
+def register() -> None:
+    def try_register(dni, nombre, apellido, email, fechaNac, rol):
+        if registrar(dni, password.value, nombre, apellido, email, fechaNac):
+            ui.notify('Registro exitoso', color='positive')
+            ui.navigate.to('/login')
+        else:
+            ui.notify('El DNI ya existe', color='negative')
+            
+    with ui.card().classes('absolute-center items-stretch'):
+        dni = ui.input('DNI').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
+        password = ui.input('Contraseña', password=True, password_toggle_button=True)
+        nombre = ui.input('Nombre/s').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
+        apellido = ui.input('Apellido').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
+        email = ui.input('Email').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
+        fnac = ui.date_input('Fecha de nacimiento').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
+        ui.button('Register', on_click=lambda: try_register(dni.value, password.value, nombre.value, apellido.value, email.value, fnac.value))
+    
+    return None    
 
 @ui.page('/login')
 def login(redirect_to: str = '/') -> RedirectResponse | None:
     if app.storage.user.get('authenticated'):
         return RedirectResponse('/')
 
-    def try_login() -> None:
-        if passwords.get(username.value) == password.value:
+    def try_login(user,passwd) -> None:        
+        if(chequearContraseña(user, passwd)):
             app.storage.user.update(username=username.value, authenticated=True)
             ui.navigate.to(redirect_to)  # go back to where the user wanted to go
         else:
             ui.notify('Nombre de Usuario o Contraseña incorrectos', color='negative')
+            
+    def go_to_register():
+        ui.navigate.to('/register')
 
     with ui.card().classes('absolute-center items-stretch'):
         username = ui.input('DNI').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
         password = ui.input('Contraseña', password=True, password_toggle_button=True).on('keydown.enter', try_login)
-        ui.button('Login', on_click=try_login)
-        ui.button('Register', on_click=None)
+        ui.button('Login', on_click=lambda: try_login(username.value, password.value))
+        ui.button('Register', on_click=go_to_register)
 
     return None
-## VOY A HACER QUE SE LOGUEEN CON SU DNI
-@ui.page('/register')
-    with ui.card().classes('absolute-center items-stretch'):
-        dni = ui.input('DNI').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
-        password = ui.input('Contraseña', password=True, password_toggle_button=True).on('keydown.enter', try_login)
-        nombre = ui.input('Nombre/s').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
-        apellido = ui.input('Apellido').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
-        email = ui.input('Email').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
-        fnac = ui.input('Fecha de nacimiento').props('autofocus').on('keydown.enter', lambda: password.run_method('focus'))
-        ui.button('Register', on_click=)
-    
+
 if __name__ in {'__main__', '__mp_main__'}:
     ui.run(storage_secret='THIS_NEEDS_TO_BE_CHANGED')
