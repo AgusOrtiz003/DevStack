d33e690f2a7a3a9b946ea2e121f1e29e23ffdd50
RicardoCantiran <rickycantiran@gmail.com>
2026-05-28 19:59:17 -0300

    Añado una condicion mas al buscar las reservas de un usuario

--- src/backend/reservas/usuario_tiene_reservas.py
+++ src/backend/reservas/usuario_tiene_reservas.py
@@ -5,6 +5,6 @@ def usuario_tiene_reservas(dni):
     """Revisa si un dni tiene reservas asociadas"""
     with sqlite3.connect('src/backend/bdd.db') as conexion:
         cursor = conexion.cursor()
-        cursor.execute('SELECT COUNT(*) FROM reservas WHERE dniPaciente=?',(dni,))
+        cursor.execute('SELECT COUNT(*) FROM reservas WHERE dniPaciente=?, estado= Pendiente',(dni,))
         resultado = cursor.fetchone()
         return resultado[0] > 0
