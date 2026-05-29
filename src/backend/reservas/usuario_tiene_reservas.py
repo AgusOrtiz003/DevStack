d3f0f25e92839e8d6cfcb19434b9cbc92f15e4d8
RicardoCantiran <rickycantiran@gmail.com>
2026-05-29 12:01:08 -0300

    me comi un and en la consulta

--- src/backend/reservas/usuario_tiene_reservas.py
+++ src/backend/reservas/usuario_tiene_reservas.py
@@ -5,6 +5,6 @@ def usuario_tiene_reservas(dni):
     """Revisa si un dni tiene reservas asociadas"""
     with sqlite3.connect('src/backend/bdd.db') as conexion:
         cursor = conexion.cursor()
-        cursor.execute('SELECT COUNT(*) FROM reservas WHERE dniPaciente=? estado=?',(dni,"Pendiente"))
+        cursor.execute('SELECT COUNT(*) FROM reservas WHERE dniPaciente=? AND estado=?',(dni,"Pendiente"))
         resultado = cursor.fetchone()
         return resultado[0] > 0
